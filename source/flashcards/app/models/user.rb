1803035d5b3ed55480cfd9df8c893b09fd56b8f6
Batman <batman@devbootcamp.com>
2014-03-14 17:21:09 -0500

    User can be authenticated

--- source/flashcards/app/models/user.rb
+++ source/flashcards/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :rounds
-  # Remember to create a migration!
+  validates :name, uniqueness: true
+
+  def self.authenticate(name, password)
+    user = User.find_by_name(name)
+    user != nil && user.password == password
+  end
 end
