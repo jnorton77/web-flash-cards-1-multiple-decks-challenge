2899b5f571a700a0c309c4f690ed4487fe889508
Apprentice <apprentice@dbc-chicago-017-67.local>
2014-03-16 08:55:13 -0500

    Refactors js

--- source/flashcards/public/js/application.js
+++ source/flashcards/public/js/application.js
@@ -2,21 +2,20 @@ $(document).ready(function() {
 
 	$("#answerdisplay").hide();
 
-		//Main request stored in a variable 
+		//Main request stored in a variable
 		var request = function(correct) {$.ajax({
-		       type: "POST",
-		       url: "/decks/:deck_name",
-		       data: "correct=" + correct + "",
-		       success: function(data) {
-	       	 render(data)
-	       	 if (data["endgame"])
-	       		 window.location.href = "/results"
-	       	 }
-		     })}
+		        type: "POST",
+		        url: "/decks/:deck_name",
+		        data: "correct=" + correct + "",
+		        success: function(data) {
+	       	  render(data)
+	       	  if (data["endgame"])
+	       		  window.location.href = "/results"
+	       	  }
+		      })}
 
 		//Render to HTML
 		var render = function(data) {
-			console.log(data["question"])
 			$("li span").text(data["question"])
 			$("#answerdisplay span").text(data["answer"])
 		}
@@ -30,29 +29,18 @@ $(document).ready(function() {
 		 	setTimeout( hide, 2000 )
 	 	}
 
+		// Get initial data
+	  request();
 
-
-	// Get initial data
-	  $.ajax({
-	 	  type:"Post",
-	 	  url: "/decks/:deck_name",
-      success: function(data) {
-     	render(data)
-			}
-	  })
-	 
 
 	  $("#card").on("submit", function() {
 	 		event.preventDefault();
 
 	 		//main logic
-	 		console.log($("#answerdisplay span").text())
-	 		if ($("#answerdisplay span").text() == $("input").val()) {
+	 		if ($("#answerdisplay span").text() == $("input").val())
 				showHideLogic(true);
-			}
-			else {
+			else
 				showHideLogic(false);
-			}
-		});	
+		});
 
 }); //end ready()
